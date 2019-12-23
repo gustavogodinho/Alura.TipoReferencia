35d3700377371dd4ef06be670dd3a22dff448091
Gustavo Godinho <gustavogodinho6@msn.com>
2019-12-22 21:09:40 -0300

    Add project files.

---
+++ AluraInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AluraInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
